4a5df7154e159b332e92c5e1d32cf66c4933e0f5
ATLAS-B28 <adityacbhambere28@gmail.com>
2024-04-30 22:10:37 +0530

    redoing the kotlin classes

--- KotlinOOP/src/main/kotlin/ClassBasic.kt
+++ KotlinOOP/src/main/kotlin/ClassBasic.kt
@@ -1,170 +1,169 @@
-package org.example
+open class SmartDevice(val name: String, val category: String) {
 
-import org.example.ab.Abstract
-import org.example.ab.Shape
-import org.example.org.example.Inject
-import org.example.org.example.MyAnnotation
+    open val deviceType = ""
+    var deviceStatus = "online"
 
-
-
-class ClassBasic(private val brand: String, private val model: String) {
-  fun startEngine() {
-      println("The $brand $model engine is starting")
-  }
-}
-/*
-* If you want to run some code during
-* object creation, use initializer
-* blocks inside the class body.
-* Initializer blocks are declared
-* with the init keyword followed by
-* curly braces. Write any code that
-* you want to run within the curly
-* braces.
-* */
-class Init(name: String) {
-    val firstProperty = "First property: $name".also(::println)
-
-    init {
-        println("First initializer block that prints $name")
+    constructor(name: String, category: String, statusCode: Int): this(name, category) {
+        deviceStatus = when(statusCode) {
+            0 -> "offline"
+            1 -> "online"
+            else -> "unknown"
+        }
+    }
+    /*var speakerVolume = 2 //this is the initial value
+        //get() = field
+        set(value) {
+            if(value in 0..100) {
+                field = value
+            }
+        }
+*/
+    fun turnOn() {
+        println("Smart Device is on")
+        turnOff()
     }
 
-    val secondProperty = "Second property: ${name.length}".also(::println)
-
-    init {
-        println("Second initializer block that prints ${name.length}")
+    fun turnOff() {
+        println("Smart Device is off")
     }
 }
 
+class SmartTvDevice(deviceName: String, deviceCategory: String):
+    SmartDevice(name = deviceName, category = deviceCategory) {
+    private var speakerVolume = 2
+        set(value) {
+            if(value in 0..100) {
+                field = value
+            }
+        }
 
-//using annotation
-@Inject
-class Customer(val name: String, val age: Int  )
+    private var channelNumber = 1
+        set(value) {
+            if(value in 0..200) {
+                field = value //if the value is between 0 and 100, set it to field
+            }
+        }
 
-@MyAnnotation("Aditya", 21)
-class MyClass {
-    fun printMessage(message: String) {
-        println(message)
+    fun increaseSpeakerVol() {
+        speakerVolume++
+        println("Speaker volume is: $speakerVolume")
     }
+
+    fun nextChannel() {
+        channelNumber++
+        println("Channel number is: $channelNumber")
+    }
+
 }
 
-class Person(val name: String) {
-    val children: MutableList<Person> = mutableListOf()
-    constructor(name: String, parent: Person)
-       /*allows the constructor to reuse
-       * initialization logic from another constructor
-       * in the class*/ : this(name) {
-           //this -> refers to the current instance
-           //of the class itself
-        parent.children.add(this)//add children to parent as person class
+class SmartTvDevice2(deviceName: String, deviceCategory: String):
+        SmartDevice(name = deviceName, category = deviceCategory) {
+            override val deviceType = "SmartTv"
+
+            var speakerVolume by Ranges(initialValue = 2, minValue = 0, maxValue = 100)
+            var channelNumber by Ranges(initialValue = 1, minValue = 0, maxValue = 200)
+        }
+
+class SmartLightDevice2(deviceName: String, deviceCategory: String):
+        SmartDevice(name = deviceName, category = deviceCategory) {
+            override val deviceType = "Smart Light"
+
+            var brightnessLevel by Ranges(initialValue = 0, minValue = 0, maxValue = 100)
+        }
+class SmartLightDevice(deviceName: String, deviceCategory: String):
+        SmartDevice(name = deviceName, category = deviceCategory) {
+
+            private var brightnessLevel = 0
+                set(value) {
+                    if(value in 0..100) {
+                        field = value
+                    }
+                }
+            fun increaseBrightness() {
+                brightnessLevel++
+                println("Brightness level is: $brightnessLevel")
+            }
+        }
+
+class SmartHome(private val smartTvDevice: SmartTvDevice,
+                private val smartLightDevice: SmartLightDevice) {
+    fun turnOnTv() {
+        smartTvDevice.turnOn()
     }
 
-    fun addChild(child: Person) {
-        children.add(child)
+    fun turnOffTv() {
+        smartTvDevice.turnOff()
     }
 
-    fun addChildren(children : List<Person>) {
-        this.children.addAll(children)
+    fun increaseTvVol() {
+        smartTvDevice.increaseSpeakerVol()
     }
 
-    fun removeChild(child: Person) {
-        children.remove(child)
-    }
-}
-
-//abstract class usage
-
-class Circle(private val radius: Double): Abstract() {
-    override fun calculateArea(): Double {
-        return Math.PI * radius * radius
-    }
-}
-/*
-* You can override a non-abstract open member with an abstract one.
-* */
-open class Polygon {
-    open fun draw() {
-        println("Drawing a polygon")
-    }
-}
-
-abstract class WildShape : Polygon() {
-    abstract override fun draw()
-}
-
-class Rhombus : WildShape() {
-    override fun draw() {
-        println("Drawing a rhombus")
-    }
-}
-
-class Circle2(override val color: String, val radius: Double): Shape {
-    override fun calArea(): Double {
-        return Math.PI * radius * radius
+    fun changeTvChannelToNext() {
+        smartTvDevice.nextChannel()
     }
 
+    fun turnOnLight() {
+        smartLightDevice.turnOn()
+    }
+
+    fun turnOffLight() {
+        smartLightDevice.turnOff()
+    }
+
+    fun increaseLightBrightness() {
+        smartLightDevice.increaseBrightness()
+    }
 }
 
 fun main() {
-    val car = ClassBasic("Toyota","Corolla")
-    car.startEngine()
-    //object
-    /*
-    * The object keyword can also be
-    * used to create anonymous objects
-    * and object expressions, which
-    * are instances of an ad-hoc class
-    * that extends a certain type or
-    * types. These are particularly
-    * useful for creating objects with
-    * a single instance and for defining
-    * anonymous classes on the fly.
-    * */
-    Logger.log("Aditya Bhambere")
+    val classBasic = SmartDevice("Atari 2600", "Video Game")
+    val smartTvDevice = SmartTvDevice("Samsung TV", "TV")
+    val smartLightDevice = SmartLightDevice("Havells Light", "Light")
+    val smartHome = SmartHome(smartTvDevice, smartLightDevice)
 
-    val init = Init("Aditya")
+    val smartTvDevice2 = SmartTvDevice2("Samsung TV", "TV")
+    val smartLightDevice2 = SmartLightDevice2("Havells Light", "Light")
 
-    println(init.firstProperty)
-    println(init.secondProperty)
+    println("Device name is: ${classBasic.name}")
+    println("Device category is: ${classBasic.category}")
+    println("Device status is: ${classBasic.deviceStatus}")
 
-    val customer = Customer("Aditya",21)
+  //  classBasic.speakerVolume = 50
+  //  println("Speaker volume is: ${classBasic.speakerVolume}")
 
-    println(customer.name)
-    println(customer.age)
+    classBasic.turnOn()
 
-    //annotation with params
-    val myAnnotate = MyClass()
-    myAnnotate.printMessage("Aditya Bhambere")
+    smartTvDevice.deviceStatus = "offline"
+    println("Device status is: ${smartTvDevice.deviceStatus}")
+    smartTvDevice.increaseSpeakerVol()
+    smartTvDevice.increaseSpeakerVol()
 
-    val person = Person("Aditya")
-    val person2 = Person("Aditya",person)
+    smartTvDevice.nextChannel()
+    smartTvDevice.nextChannel()
 
-    println(person.children)
-    println(person2.children)
+    smartLightDevice.deviceStatus = "online"
+    println("Device status is: ${smartLightDevice.deviceStatus}")
+    smartLightDevice.increaseBrightness()
 
-    person.name
+    smartHome.turnOnTv()
+    smartHome.turnOffTv()
+    smartHome.increaseTvVol()
+    smartHome.changeTvChannelToNext()
 
-    person.addChild(person2)
-    person.children
-    println(person.children[0].name)
-    person.addChildren(listOf(person2))
-    person.children
-    println(person.children[1].name)
-    person.removeChild(person2)
-    person.children
-    println(person.children[0].name)
-    for (child in person.children) {
-        println(child.name)
-    }
+    smartHome.turnOnLight()
+    smartHome.turnOffLight()
+    smartHome.increaseLightBrightness()
 
-    val circle = Circle(9.3)
-    println(circle.calculateArea())
+    smartTvDevice2.deviceStatus = "offline"
+    println("Device status is: ${smartTvDevice2.deviceStatus}")
+    smartTvDevice2.speakerVolume = 50
+    println("Speaker volume is: ${smartTvDevice2.speakerVolume}")
+    smartTvDevice2.channelNumber = 100
+    println("Channel number is: ${smartTvDevice2.channelNumber}")
 
-    val rhombus = Rhombus()
-    rhombus.draw()
-
-    val circle2 = Circle2("blue",9.3)
-    println(circle2.color)
-    println(circle2.radius)
-    println(circle2.calArea())
+    smartLightDevice2.deviceStatus = "online"
+    println("Device status is: ${smartLightDevice2.deviceStatus}")
+    smartLightDevice2.brightnessLevel = 50
+    println("Brightness level is: ${smartLightDevice2.brightnessLevel}")
 }
